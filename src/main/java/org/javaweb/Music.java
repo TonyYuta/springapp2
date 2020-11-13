524c63b6baa4acf86ac6b42193455890230eccc2
TonyYuta <tonykagaya@gmail.com>
2020-11-12 16:30:06 -0800

    IoC was implemented

---
+++ src/main/java/org/javaweb/Music.java
@@ -0,0 +1,5 @@
+package org.javaweb;
+
+public interface Music {
+    String getSong();
+}
---
+++ src/main/java/org/javaweb/SoulMusic.java
@@ -0,0 +1,8 @@
+package org.javaweb;
+
+public class SoulMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Georgia in my mind";
+    }
+}
