ce42f33fb21e18222c5c657055ad3f69e7b579bf
TonyYuta <tonykagaya@gmail.com>
2020-11-13 15:06:48 -0800

    6: modified into music list

--- src/main/java/org/javaweb/MusicPlayer.java
+++ src/main/java/org/javaweb/MusicPlayer.java
@@ -1,20 +1,13 @@
 package org.javaweb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+
     private String name;
     private int volume;
-
-    // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public MusicPlayer() {}
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
 
     public String getName() {
         return name;
@@ -32,8 +25,13 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
 
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+    public void playMusicList() {
+        for(Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
