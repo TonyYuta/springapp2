bcc34c75e773356daa2375a863dd9920a652ce22
TonyYuta <tonykagaya@gmail.com>
2020-11-14 16:54:53 -0800

    10: Autowired

--- src/main/java/org/javaweb/MusicPlayer.java
+++ src/main/java/org/javaweb/MusicPlayer.java
@@ -1,17 +1,29 @@
 package org.javaweb;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MusicPlayer {
-    private Music music;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
+    private RapMusic rapMusic;
+    private SoulMusic soulMusic;
 
-    // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic, RapMusic rapMusic, SoulMusic soulMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
+        this.rapMusic = rapMusic;
+        this.soulMusic = soulMusic;
     }
 
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+    public String playMusic() {
+        return "Playing: " + classicalMusic.getSong();
+//        System.out.println("Playing: " + classicalMusic.getSong());
+//        System.out.println("Playing: " + rockMusic.getSong());
+//        System.out.println("Playing: " + rapMusic.getSong());
+//        System.out.println("Playing: " + soulMusic.getSong());
+
     }
 }
